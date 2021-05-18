aecf69cc192d9e0d331b5ec33a35f0498e9d67cc
jackhsu87 <80099562+jackhsu87@users.noreply.github.com>
2021-05-18 18:58:30 +0800

    21

--- defect_ticket_gen.py
+++ defect_ticket_gen.py
@@ -102,4 +102,4 @@ def ticket_gen(input_file):
             ticket.save('{}.docx'.format(doc_name))
 
 if __name__ == '__main__':
-    ticket_gen('W20 Defect Tickets Gen.xlsx')
+    ticket_gen('W21 Defect Tickets Gen.xlsx')
